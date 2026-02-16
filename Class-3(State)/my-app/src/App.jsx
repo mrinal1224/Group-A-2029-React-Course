58b961c742ffcc5aac06ff4d5797099ebd1237d1
mrinal1224 <iammrinalkc@gmail.com>
2026-02-16 16:13:43 +0530

    push

--- Class-3(State)/my-app/src/App.jsx
+++ Class-3(State)/my-app/src/App.jsx
@@ -1,26 +1,17 @@
 import { useState } from "react";
+import Counter from "./components/Counter";
+import SignupForm from "./components/SignupForm";
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  console.log("Re-Rendered");
-
-  function increment() {
-    setCount(count + 1);
-  }
-
-  function decrement() {
-    if (count > 0) {
-      setCount(count - 1);
-    }
-  }
-
   return (
-    <>
-      <button onClick={increment}>Increment</button>
-      <h2>{count}</h2>
-      <button onClick={decrement}>Decrement</button>
-    </>
+    <div>
+      {/* <Counter /> */}
+      <SignupForm/>
+     
+    </div>
   );
 }
 
---
+++ Class-3(State)/my-app/src/components/SignupForm.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+
+function SignupForm() {
+  const [name, setName] = useState("");
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function submit(e) {
+    e.preventDefault();
+
+    let obj = {
+      name: name,
+      userName: userName,
+      email: email,
+      pass: password,
+    };
+    console.log(obj);
+
+    setName('')
+    setUserName('')
+    setPassword('')
+    setEmail('')
+  }
+
+  return (
+    <div>
+      <form>
+        <label>Name</label>
+        <input type="text" onChange={(e) => setName(e.target.value)} />
+
+        <label>UserName</label>
+        <input type="text" onChange={(e) => setUserName(e.target.value)} />
+
+        <label>Email</label>
+        <input type="email" onChange={(e) => setEmail(e.target.value)} />
+
+        <label>Password</label>
+        <input type="password" onChange={(e) => setPassword(e.target.value)} />
+
+        <button onClick={submit}>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default SignupForm;
