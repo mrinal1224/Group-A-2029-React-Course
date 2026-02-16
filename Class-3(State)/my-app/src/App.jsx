e607982759f70c56a4e256abe3070fc356c7b3a5
mrinal1224 <iammrinalkc@gmail.com>
2026-02-16 15:39:30 +0530

    push

---
+++ Class-3(State)/my-app/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  console.log("Re-Rendered");
+
+  function increment() {
+    setCount(count + 1);
+  }
+
+  function decrement() {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  }
+
+  return (
+    <>
+      <button onClick={increment}>Increment</button>
+      <h2>{count}</h2>
+      <button onClick={decrement}>Decrement</button>
+    </>
+  );
+}
+
+export default App;
