b3462737f8e108caabc7aa704a1caf6c082c7769
mrinal1224 <iammrinalkc@gmail.com>
2026-02-27 16:33:40 +0530

    push movies Page

--- Class-5 (imdb-Clone)/src/components/MovieCard.jsx
+++ Class-5 (imdb-Clone)/src/components/MovieCard.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function MovieCard() {
+function MovieCard({title , posterUrl}) {
   return (
-    <div className='h-90 w-60 bg-cover ' style={{backgroundImage:`url(https://images-cdn.ubuy.co.in/68901f6ae49b8c404602f009-the-batman-movie-poster-glossy-quality.jpg)`}}>
-
+    <div className='h-90 w-60 bg-cover ' style={{backgroundImage:`url(https://image.tmdb.org/t/p/w500/${posterUrl})`}}>
+      <p className=''>{title}</p>
     </div>
   )
 }
--- Class-5 (imdb-Clone)/src/components/Movies.jsx
+++ Class-5 (imdb-Clone)/src/components/Movies.jsx
@@ -1,27 +1,38 @@
-import React from 'react'
-import Banner from './Banner'
-import MovieCard from './MovieCard'
+import React, { useEffect, useState } from "react";
+import Banner from "./Banner";
+import MovieCard from "./MovieCard";
+import axios from "axios";
+
+//  https://api.themoviedb.org/3/movie/popular?api_key=3aec63790d50f3b9fc2efb4c15a8cf99&language=en-US&page=3
 
 function Movies() {
+  // get the data from the url
+  // in the app and show the data on the console
+
+const [movies , setMovies] =   useState([])
+
+  useEffect(() => {
+    async function getMovies() {
+      let response = await axios.get(
+        `https://api.themoviedb.org/3/movie/popular?api_key=3aec63790d50f3b9fc2efb4c15a8cf99&language=en-US&page=3`
+      );
+      setMovies(response.data.results)
+    }
+
+    getMovies()
+  }, []);
+
   return (
     <div>
-        <Banner/>
+      <Banner />
 
-        <div className='mt-10 flex gap-6 flex-wrap justify-evenly'>
-
-        <MovieCard/>
-        <MovieCard/>  
-        <MovieCard/>   
-        <MovieCard/>
-        <MovieCard/>
-        <MovieCard/>
-        <MovieCard/>
-        <MovieCard/>
-        <MovieCard/>
-        <MovieCard/>
-        </div>
+      <div className="mt-10 flex gap-6 flex-wrap justify-evenly">
+       {movies.map((movieObj)=>{
+        return <MovieCard title={movieObj.title} posterUrl={movieObj.poster_path}/>
+       })}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Movies
+export default Movies;
