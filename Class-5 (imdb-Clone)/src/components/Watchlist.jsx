0a20b84497f97280d6f85f76ba73e24d9bddb43f
mrinal1224 <iammrinalkc@gmail.com>
2026-03-16 16:02:57 +0530

    filter implemented

--- Class-5 (imdb-Clone)/src/components/Watchlist.jsx
+++ Class-5 (imdb-Clone)/src/components/Watchlist.jsx
@@ -3,6 +3,12 @@ import { genreids } from "../genres";
 
 function Watchlist({ watchlist }) {
   const [genreList, setGenreList] = useState([]);
+  const [currentGenre, setCurrentGenre] = useState("All Genres");
+
+  function changeGenre(genre) {
+    setCurrentGenre(genre);
+  }
+  console.log(currentGenre);
 
   useEffect(() => {
     let genreArr = watchlist.map((movieObj) => genreids[movieObj.genre_ids[0]]);
@@ -13,12 +19,12 @@ function Watchlist({ watchlist }) {
 
   return (
     <div className="overflow-hidden rounded-lg border border-gray-200 m-8">
-      
       {/* Centered Genre List */}
       <div className="flex flex-wrap justify-center gap-4 py-5 bg-gray-50 border-b">
         {genreList.map((genre) => (
           <div
             key={genre}
+            onClick={() => changeGenre(genre)}
             className="px-6 py-2 border rounded-xl bg-blue-500 text-white font-bold text-sm cursor-pointer hover:bg-blue-600 transition-colors"
           >
             {genre}
@@ -40,37 +46,46 @@ function Watchlist({ watchlist }) {
         </thead>
 
         <tbody className="divide-y divide-gray-200 bg-white">
-          {watchlist.map((movieObj) => (
-            <tr key={movieObj.id} className="hover:bg-gray-50 transition-colors">
-              <td className="px-6 py-4">
-                <div
-                  className="h-20 w-14 bg-cover bg-center rounded-md shadow-sm"
-                  style={{
-                    backgroundImage: `url('https://image.tmdb.org/t/p/w500/${movieObj.poster_path}')`,
-                  }}
-                ></div>
-              </td>
-              <td className="px-6 py-4 font-medium text-gray-900 text-lg">
-                {movieObj.title}
-              </td>
-              <td className="px-6 py-4">
-                <span className="flex items-center">
-                  ⭐ {movieObj.vote_average}
-                </span>
-              </td>
-              <td className="px-6 py-4">{movieObj.popularity}</td>
-              <td className="px-6 py-4">
-                <span className="px-3 py-1 bg-blue-100 text-blue-700 rounded-full text-xs font-bold">
-                  {genreids[movieObj.genre_ids[0]]}
-                </span>
-              </td>
-              <td className="px-6 py-4">
-                <button className="text-red-500 hover:text-red-700 font-semibold cursor-pointer">
-                  Delete
-                </button>
-              </td>
-            </tr>
-          ))}
+          {watchlist
+            .filter(
+              (movieObj) =>
+                currentGenre === "All Genres" ||
+                genreids[movieObj.genre_ids[0]] === currentGenre
+            )
+            .map((movieObj) => (
+              <tr
+                key={movieObj.id}
+                className="hover:bg-gray-50 transition-colors"
+              >
+                <td className="px-6 py-4">
+                  <div
+                    className="h-20 w-14 bg-cover bg-center rounded-md shadow-sm"
+                    style={{
+                      backgroundImage: `url('https://image.tmdb.org/t/p/w500/${movieObj.poster_path}')`,
+                    }}
+                  ></div>
+                </td>
+                <td className="px-6 py-4 font-medium text-gray-900 text-lg">
+                  {movieObj.title}
+                </td>
+                <td className="px-6 py-4">
+                  <span className="flex items-center">
+                    ⭐ {movieObj.vote_average}
+                  </span>
+                </td>
+                <td className="px-6 py-4">{movieObj.popularity}</td>
+                <td className="px-6 py-4">
+                  <span className="px-3 py-1 bg-blue-100 text-blue-700 rounded-full text-xs font-bold">
+                    {genreids[movieObj.genre_ids[0]]}
+                  </span>
+                </td>
+                <td className="px-6 py-4">
+                  <button className="text-red-500 hover:text-red-700 font-semibold cursor-pointer">
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
         </tbody>
       </table>
     </div>
