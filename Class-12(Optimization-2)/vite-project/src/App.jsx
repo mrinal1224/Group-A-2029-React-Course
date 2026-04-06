dc6068856d140a6421823eec4ce8d53c57d66051
mrinal1224 <iammrinalkc@gmail.com>
2026-04-06 16:07:31 +0530

    useRef

--- Class-12(Optimization-2)/vite-project/src/App.jsx
+++ Class-12(Optimization-2)/vite-project/src/App.jsx
@@ -3,13 +3,14 @@ import './App.css'
 import AnalyticsWithMemo from './components/AnalyticsWithMemo'
 import AnalyticsWithoutMemo from './components/AnalyticsWithoutMemo'
 import Memo from './components/Memo'
+import Ref from './components/Ref'
 
 function App() {
 
 
   return (
     <>
-     <AnalyticsWithMemo/>
+      <Ref/>
     </>
   )
 }
---
+++ Class-12(Optimization-2)/vite-project/src/components/Ref.jsx
@@ -0,0 +1,29 @@
+// useRef
+
+import React from "react";
+import { useRef } from "react";
+import { useState } from "react";
+
+function Ref() {
+  const [text, setText] = useState("");
+
+ const inputRef =  useRef(null)
+
+ console.log(inputRef)
+
+  function reset() {
+    setText("");
+    inputRef.current.focus()
+    inputRef.current.style.backgroundColor = 'red'
+    
+  }
+
+  return (
+    <div>
+      <input ref={inputRef} value={text} onChange={(e) => setText(e.target.value)} />
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+}
+
+export default Ref;
